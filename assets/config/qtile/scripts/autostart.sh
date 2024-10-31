77d4a090c0ca648001677f867bc5a22231a493cc
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-10-31 13:29:58 +0000

    Update autostart.sh

--- assets/config/qtile/scripts/autostart.sh
+++ assets/config/qtile/scripts/autostart.sh
@@ -13,7 +13,12 @@ if [ -s "$CONFIG_FILE" ]; then
 
   # Apply the wallpaper using feh
   feh --bg-scale "$SELECTED_WALLPAPER" &
+elif [ ! -f "$CONFIG_FILE" ]; then
+        echo /home/$USER/.config/wallpapers/default/6xVGpvY-arch-linux-wallpaper.png > /home/$USER/.config/$DESKTOP_SESSION/.selected_wallpaper
+        SELECTED_WALLPAPER=$(cat "$CONFIG_FILE")
+        feh --bg-scale "$SELECTED_WALLPAPER" &
 fi
+
 
 source /home/$USER/.config/qtile/scripts/sounds.sh
 if [[ ! -z "$login_sound" && $active_sounds = yes ]]; then
